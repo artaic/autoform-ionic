1b362606eb897598ff4f636b6a08926b8b19a70a
Michael Trachtman <myktra@gmail.com>
2015-12-31 09:03:27 -0500

    version bumps

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
   name: "artaic:autoform-ionic",
   summary: "Ionic theme for Autoform",
-  version: "0.1.7",
+  version: "0.1.8",
   git: "https://github.com/meteoric/autoform-ionic.git"
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom("1.0");
+  api.versionsFrom("1.2.1");
   api.use(["templating", "underscore"], "client");
-  api.use("aldeed:autoform@5.7.1");
+  api.use("aldeed:autoform@5.8.1");
   api.addFiles([
     "ionic.html",
     "ionic.css",
